98ee271158afcc814a6c458e757d1034d0efc5af
lijianhui <lijianhui@shcem.com>
2016-12-02 18:09:57 +0800

    commit

--- pages/main/main.js
+++ pages/main/main.js
@@ -7,6 +7,7 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+   
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
@@ -24,6 +25,12 @@ Page({
         currentTab: e.target.dataset.current  
       })  
     } 
+  },
+     //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../mainDetail/maindetail'
+    })
   }
 
  
--- pages/mall/mall.js
+++ pages/mall/mall.js
@@ -1,5 +1,14 @@
 Page({
-  data: {
+  data: {   
+    "category":[
+    {"id":"1","name":"LDPE","valueId":1},
+    {"id":"2","name":"HDPE","valueId":5},
+    {"id":"3","name":"LLDPE","valueId":6},
+    {"id":"4","name":"PP均聚","valueId":20},
+    {"id":"5","name":"PP共聚","valueId":21},
+    {"id":"6","name":"PVC","valueId":31},
+    {"id":"7","name":"全部","valueId":0}
+  ]
 
   },
   // tab切换
@@ -12,7 +21,11 @@ Page({
         currentTab: e.target.dataset.current  
       })  
     } 
-  }
+  },
 
- 
+   bindViewTap: function() {
+    wx.navigateTo({
+      url: '../malldetail/malldetail'
+    })
+  }
 })
